cfdee067d709cb8aab995954652d8ffd4a336131
Alexis Okuwa <wojonstech@gmail.com>
2014-01-30 11:14:08 -0800

    still working on the new io model for this

--- src/core/coStreamSocket.php
+++ src/core/coStreamSocket.php
@@ -2,6 +2,13 @@
 
 class coStreamSocket {
     protected $stream;
+    protected $is_readable         = False;
+    protected $is_writable         = False;
+    protected $read_buffer         = "";
+    protected $write_buffer        = "";
+    protected $default_read_buffer = 1024;
+    protected $is_error;
+    protected $fd;
     
     function __construct($stream=null) {
         if(is_resource($stream)) {
@@ -18,36 +25,65 @@ class coStreamSocket {
         return $this->stream;
     }
     
-    function canRead() {
-        $stream = array($this->stream);
-        $write  = Null;
-        $except = Null;
-        //print "waiting";
-        if(stream_select($stream, $write, $except, 0, 0) > 0) {
+    function setReadBuffer($bytes) {
+        $this->read_buffer_max = $bytes;
+    }
+    
+    function availReadBuffer() {
+        if($this->read_buffer_max == -1) {
             return True;
+        }
+        elseif($this->strlen($this->read_buff) < $this->read_buffer_max) {
+            return $this->read_buff_max - strlen($this->read_buffer);
         }
         return False;
     }
     
-    function canWrite() {
-        $stream = array($this->stream);
-        $read   = Null;
-        $except = Null;
-        if(stream_select($read, $stream, $except, 0) > 0) {
-            return True;
+    function readEvent() {
+        yield;
+        while(True) {
+            $availReadBuffer = $this->availReadBuffer();
+            if($this->is_readable == True && ($availReadBuffer == True || $availReadBuffer > 0) {
+                $this->read_buffer .= fread($this->stream, ((is_int($availReadBuffer)) ? $availReadBuffer : $this->default_read_buffer));
+                $this->is_readable  = False;
+            }
+            yield (new systemCall(function($task, $scheduler) {
+                $scheduler->addStream2Watch(array($this->fd, $this->stream));
+            }));
         }
         return False;
     }
     
-    function isExcept() {
-        $stream = Null;
-        $write  = Null;
-        $except = array($this->stream);
-        //print "waiting";
-        if(stream_select($stream, $write, $except, 0, 0) > 0) {
-            return True;
+    function writeEvent() {
+        yield;
+        while(True)
+            if($this->is_writeable == True && !empty($this->write_buffer)) {
+                
+                $written            = fwrite($this->stream, $this->write_buffer);
+                if($written === False) {
+                    $this->client_closed = True;
+                    break;
+                }
+                elseif($written < strlen($this->write_buffer)) {
+                    $this->write_buffer = substr($this->write_buffer, $written);
+                } else {
+                    $this->write_buffer = "";
+                }
+                
+                
+                $this->is_writable  = False;
+            }
+            yield;
         }
-        return False;
+    }
+    
+    function setFD($fd) {
+        $this->fd = $fd;
+        return True;
+    }
+    
+    function getFD() {
+        return $this->fd;
     }
 }
 
---
+++ src/core/fileDescriptor.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * 
+ * Implentaon of a file descriptor table that will exist in each task
+ * optimzed for getting the resource and not figuring out what type of
+ * resoruce it is
+ * 
+ */
+
+class fileDescriptor {
+    
+    private $table = array();
+    private $type  = array();
+    
+    static $next_fd = 0; //this will be global for all kernels running should be fixed later
+    
+    function __construct() {
+        // will build this function out as needed
+        $this->add(Null); //stdin
+        $this->add(Null); //stdout
+        $this->add(Null); //stderr
+    }
+    
+    function __destruct() {
+        foreach($this->table as $dex=>$dat) {
+            unset($this->table[$dex]);
+        }
+        
+        foreach($this->type as $dex=>$dat) {
+            foreach($dat as $dex2=>$dat2) {
+                unset($this->type[$dex][$dex2]);
+            }
+        }
+    }
+    
+    function getNextFd() {
+        return static::$next_fd++;
+    }
+    
+    function add(&$fd, $type=null) {
+        $type = is_null($type) ? 'main' : $type;
+        $fd_id                     = $this->getNextFd();
+        $this->table[$fd_id]       =& $fd;
+        $this->type[$type][$fd_id] =& $this->table[$fd_id];
+        return $fd_id;
+    }
+    
+    function del($fd) {
+        if(isset($this->table[$fd])) {
+            foreach($this->type as $dex=>$dat) {
+                return unset($this->table[$fd], $this->type[$dex][$fd]);
+            }
+            return unset($this->table[$fd]); //only used if its rouge;
+        }
+    }
+    
+    function &get($fd) {
+        return $this->table[$fd];
+    }
+}
+
+?>
